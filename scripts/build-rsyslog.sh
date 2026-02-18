512701561dcdfd85bc38c92f47c2563e4656ea7b
Aethersailor <22260104+Aethersailor@users.noreply.github.com>
2026-02-18 14:21:26 +0800

    fix: disable impstats-push to avoid protoc-c build failure

--- scripts/build-rsyslog.sh
+++ scripts/build-rsyslog.sh
@@ -25,6 +25,7 @@ autoreconf -fvi
   --disable-imfile \
   --disable-imzmq \
   --disable-libdbi \
+  --disable-impstats-push \
   --disable-clickhouse \
   --disable-elasticsearch \
   --disable-gssapi \
